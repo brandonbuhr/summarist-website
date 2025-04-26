12af6179cb3a149edd5910182654719492469578
brandonbuhr <brandonbuhr35@gmail.com>
2025-04-26 15:57:08 -0500

    added firebase sdk config and env file for keys

--- firebase/firebaseClient.ts
+++ firebase/firebaseClient.ts
@@ -1,12 +1,13 @@
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  appId: "YOUR_FIREBASE_APP_ID",
+  apiKey: "FIREBASE_API_KEY",
+  authDomain: "FIREBASE_AUTH_DOMAIN",
+  projectId: "FIREBASE_PROJECT_ID",
+  storageBucket: "FIREBASE_STORAGE_BUCKET",
+  messagingSenderID: "FIREBASE_MESSAGING_SENDER_ID",
+  appId: "FIREBASE_APP_ID"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
