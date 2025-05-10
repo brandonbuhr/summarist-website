3f68b783cb406879564167c996621f7515370219
brandonbuhr <brandonbuhr35@gmail.com>
2025-05-09 19:04:03 -0500

    added styling to sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,15 +1,23 @@
 export default function Sidebar() {
   return (
-    <>
-      <div>
-        <h1>Summarist</h1>
-        <ul>
+    <div className="sidebar">
+      <div className="sidebar-top">
+        <h1 className="sidebar-logo">📘 Summarist</h1>
+        <ul className="sidebar-nav">
           <li>For you</li>
           <li>My Library</li>
           <li>Highlights</li>
           <li>Search</li>
         </ul>
       </div>
-    </>
+
+      <div className="sidebar-bottom">
+        <ul className="sidebar-nav">
+          <li>Settings</li>
+          <li>Help & Support</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </div>
   );
 }
