805106ec199fa9aa5deb32971b8e29554f99864b
brandonbuhr <brandonbuhr35@gmail.com>
2025-05-30 18:47:17 -0500

    responsiveness added

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -18,36 +18,49 @@ export default function Sidebar() {
   };
 
   return (
-    <div className="sidebar">
-      <div className="sidebar-top">
-        <h1 className="sidebar-logo">📘 Summarist</h1>
-        <ul className="sidebar-nav">
-          <Link href="/for-you">
-            <li>For you</li>
-          </Link>
-          <Link href="/my-library">
-            <li>My Library</li>
-          </Link>
-          <Link href="/choose-plan">
-            <li>Subscription</li>
-          </Link>
-          <Link href="/for-you">
-            <li>Search</li>
-          </Link>
-        </ul>
-      </div>
+    <>
+      <input type="checkbox" id="sidebar-toggle" className="sidebar-checkbox" />
 
-      <div className="sidebar-bottom">
-        <ul className="sidebar-nav">
-          <Link href="/settings">
-            <li>Settings</li>
-          </Link>
-          <li style={{ cursor: "not-allowed" }}>Help & Support</li>
-          <li onClick={handleLogout} style={{ cursor: "pointer" }}>
-            Logout
-          </li>
-        </ul>
+      <label htmlFor="sidebar-toggle" className="sidebar-hamburger">
+        <span className="bar" />
+        <span className="bar" />
+        <span className="bar" />
+      </label>
+
+      <div className="sidebar-wrapper">
+        <div className="sidebar">
+          <div className="sidebar-top">
+            <h1 className="sidebar-logo">📘 Summarist</h1>
+
+            <ul className="sidebar-nav">
+              <Link href="/for-you">
+                <li>For you</li>
+              </Link>
+              <Link href="/my-library">
+                <li>My Library</li>
+              </Link>
+              <Link href="/choose-plan">
+                <li>Subscription</li>
+              </Link>
+              <Link href="/for-you">
+                <li>Search</li>
+              </Link>
+            </ul>
+          </div>
+
+          <div className="sidebar-bottom">
+            <ul className="sidebar-nav">
+              <Link href="/settings">
+                <li>Settings</li>
+              </Link>
+              <li style={{ cursor: "not-allowed" }}>Help & Support</li>
+              <li onClick={handleLogout} style={{ cursor: "pointer" }}>
+                Logout
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
