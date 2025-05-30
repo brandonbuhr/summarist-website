c7ccc01d3401e4149a1e7c3e72ea68bf542256cf
brandonbuhr <brandonbuhr35@gmail.com>
2025-05-29 20:46:50 -0500

    updated route.ts

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -2,13 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 import admin from "firebase-admin";
 
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY!.replace(/\\n/g, "\n")
+);
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID!,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
